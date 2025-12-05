ee2201ae0f04f453d2324200e098ac19d61ab2d6
Nikita Shpulin <shpulinn@mail.ru>
2025-12-05 10:12:39 +0200

    new SoundsButtonUI  script

---
+++ Assets/BallBlastSF/Scripts/SoundsButtonUI.cs
@@ -0,0 +1,17 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class SoundsButtonUI : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI buttonText;
+
+    private void Start()
+    {
+        SoundManager.OnMute += OnMute;
+    }
+
+    private void OnMute(bool mute)
+    {
+        buttonText.fontStyle = mute ? FontStyles.Strikethrough : FontStyles.Normal;
+    }
+}
