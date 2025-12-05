5c867157c183811125b7cc59f5ad46ac95f8f9b7
Nikita Shpulin <shpulinn@mail.ru>
2025-12-05 10:12:57 +0200

    MenuPanelUI script

---
+++ Assets/BallBlastSF/Scripts/MenuPanelUI.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+using DG.Tweening;
+
+public class MenuPanelUI : MonoBehaviour
+{
+    private void Start()
+    {
+        transform.localPosition = new Vector3(0, -1100, 0);
+        transform.DOLocalMoveY(0, 0.5f).SetEase(Ease.InOutSine);
+    }
+
+    public void StartGame()
+    {
+        transform.DOLocalMoveY(-1100, 0.5f).SetEase(Ease.InOutSine)
+            .OnComplete(() => gameObject.SetActive(false));
+    }
+
+    private void OnEnable()
+    {
+        transform.DOLocalMoveY(0, 0.5f).SetEase(Ease.InOutSine);
+    }
+
+    public void ExitGame() => Application.Quit();
+}
